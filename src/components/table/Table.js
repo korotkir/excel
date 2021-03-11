68c267eafcedaa47adb29f277f73d54a67a5b891
tre3ubec <ekoroteev27@yandex.ru>
2021-03-11 18:13:06 +0300

    added row-resize class & resize style (42)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,6 +3,13 @@ import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponents {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    })
+  }
+
   toHTML() {
     return createTable(20)
   }
