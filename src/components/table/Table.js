133f31aa4430a75ce2e278d202310b3643a96cb1
tre3ubec <ekoroteev27@yandex.ru>
2021-03-15 13:58:31 +0300

    final resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
-import {ExcelComponents} from '@core/ExcelComponents';
-import {createTable} from '@/components/table/table.template';
-import {$} from '@core/dom';
+import {ExcelComponents} from '@core/ExcelComponents'
+import {createTable} from '@/components/table/table.template'
+import {resizeHandler} from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.function';
 
 export class Table extends ExcelComponents {
   static className = 'excel__table'
@@ -16,35 +17,9 @@ export class Table extends ExcelComponents {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target)
-      // const $parent = $resizer.$el.parentNode  // bad!
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-      const type = $resizer.data.resize
-
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-
-      document.onmousemove = e => {
-        console.log('mousemove')
-        if (type === 'col') {
-          // Получаем расстояния от исходного края до сдвига в px
-          const delta = e.pageX - coords.right
-          const value = coords.width + delta
-          $parent.css({width: value + 'px'})
-          cells.forEach(el => el.style.width = value + 'px')
-        } else if (type === 'row') {
-          // Получаем расстояния от исходного края до сдвига в px
-          const delta = e.pageY - coords.bottom
-          const value = coords.height + delta
-          $parent.css({height: value + 'px'})
-          cells.forEach(el => el.style.height = value + 'px')
-        }
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null
-      }
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
   }
 }
+
