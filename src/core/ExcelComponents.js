da2bf323e227cacfd4a26e04e4ba7715288f35c7
tre3ubec <ekoroteev27@ya.ru>
2021-03-24 16:06:54 +0300

    improving the framework (60)

--- src/core/ExcelComponents.js
+++ src/core/ExcelComponents.js
@@ -5,10 +5,12 @@ export class ExcelComponents extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.unsubscribers = []
 
     this.prepare()
   }
 
+  // Настриваем наш компонент до init
   prepare() {}
 
   // Возвращает шаблон компонента
@@ -16,11 +18,27 @@ export class ExcelComponents extends DomListener {
     return ''
   }
 
+  // Уведомляем слушатилей про события event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
   init() {
     this.initDOMListeners()
   }
 
+  // Удаляем компонент
+  // Чистим слушатели
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
