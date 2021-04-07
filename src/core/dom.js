b5616c8f32a4d51a1897a1f10903d2f476cb5e2c
tre3ubec <ekoroteev27@ya.ru>
2021-04-07 17:53:15 +0300

    parse cells

--- src/core/dom.js
+++ src/core/dom.js
@@ -18,7 +18,7 @@ class Dom {
   }
 
   text(text) {
-    if (typeof text === 'string') {
+    if (typeof text !== 'undefined') {
       this.$el.textContent = text
       return this
     }
@@ -109,6 +109,15 @@ class Dom {
     this.$el.focus()
     return this
   }
+
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value)
+      return this
+    }
+    return this.$el.getAttribute(name)
+  }
+
 
   addClass(className) {
     this.$el.classList.add(className)
