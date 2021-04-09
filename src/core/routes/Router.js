4f16cd9d4c9fed6d348c0a32a662d7cbaceb7aff
tre3ubec <ekoroteev27@ya.ru>
2021-04-09 14:14:58 +0300

    page refactor

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,5 +1,4 @@
 import {$} from '@core/dom'
-import {ActiveRoute} from '@core/routes/ActiveRoute'
 
 export class Router {
   constructor(selector, routes) {
@@ -20,9 +19,11 @@ export class Router {
   }
 
   changePageHandler() {
-    console.log(ActiveRoute.path)
-		  console.log(('param', ActiveRoute.param))
-		  this.$placeholder.html('<h1>' + ActiveRoute.path + '</h1>')
+  		const Page = this.routes.excel
+		  const page = new Page()
+		  this.$placeholder.append(page.getRoot())
+
+		  page.afterRender()
   }
 
   destroy() {
