5ed56029e017a52181f6c48549e0869ca6b357a7
tre3ubec <ekoroteev27@ya.ru>
2021-04-12 13:27:11 +0300

    fixed bug

--- src/pages/dashboard.functions.js
+++ src/pages/dashboard.functions.js
@@ -12,7 +12,7 @@ function getAllKeys() {
   const keys = []
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i)
-    if (key.includes('excel')) {
+    if (!key.includes('excel')) {
     	continue
     }
     keys.push(key)
